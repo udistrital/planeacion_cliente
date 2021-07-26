5686c0480d4c57a4b1d7f3fa1f3f9878f26af9c2
ndmc98 <norbey.munoz98@gmail.com>
2021-07-26 09:34:46 -0500

    feat: se modifica el logo de la aplicación por motivo de prueba y apropiación del cliente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   autenticacion: true,
   notificaciones: false,
   menuApps: false,
-  appname: 'alternancia',
+  appname: 'ud',
   appMenu: 'alternancia',
   NUXEO: {
     PATH: 'https://documental.udistrital.edu.co/nuxeo/',
