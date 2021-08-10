f7dc42e6d81a56f87aa7d6614ce49d596f24edc5
fabianbarreto02 <fabibeto11@hotmail.com>
2021-08-10 09:22:13 -0500

    management: Ajuste del token de autenticación en ambiente de test y url de redirección

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -22,9 +22,9 @@ export const environment = {
     CLIENTE_ID: 'uNb7k8hXSICJqmZzPqrKE6sDM6Ea',
     RESPONSE_TYPE: 'id_token token',
     SCOPE: 'openid email',
-    REDIRECT_URL: 'https://alternancia.portaloas.udistrital.edu.co',
+    REDIRECT_URL: 'https://pruebasplaneacion.portaloas.udistrital.edu.co',
     SIGN_OUT_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oidc/logout',
-    SIGN_OUT_REDIRECT_URL: 'https://alternancia.portaloas.udistrital.edu.co',
+    SIGN_OUT_REDIRECT_URL: 'https://pruebasplaneacion.portaloas.udistrital.edu.co',
     AUTENTICACION_MID: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol',
   },
 };
