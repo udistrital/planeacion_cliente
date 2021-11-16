5aef5cb05f694839d5e914afbc7e059bb2b89618
brayaned <brayaned9@gmail.com>
2021-11-15 23:37:13 -0500

    fix: Ajuste de dahsboard

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,20 @@ const menu = [
     { Nombre: "Construir", Icono: 'input', Url: "pages/plan/construir-plan", TipoOpcion: "Menú", Opciones: null },
     { Nombre: "Consultar", Icono: 'search', Url: "pages/plan/consultar-plan", TipoOpcion: "Menú", Opciones: null }
     ]},
-  { Nombre: "Formulación", Icono: "forward", Url: "pages/formulacion", Opciones: null },
-  { Nombre: "Evaluación", Icono: "done_all", Url: "pages/evaluacion", Opciones: null },
-  { Nombre: "Seguimiento", Icono: "find_in_page", Url: "pages/seguimiento", Opciones: null },  
+  {Nombre: "Plan de Acción", Icono: "assignment", Url: null, Opciones: [
+    { Nombre: "Funcionamiento", Icono: "forward", Url: null, Opciones: [
+      { Nombre: "Formulación", Icono: "forward", Url: "pages/formulacion", Opciones: null },
+      { Nombre: "Evaluación", Icono: "done_all", Url: "pages/evaluacion", Opciones: null },
+      { Nombre: "Seguimiento", Icono: "find_in_page", Url: "pages/seguimiento", Opciones: null },  
+    ] },
+    { Nombre: "Inversion", Icono: "done_all", Url: null, Opciones: [
+      { Nombre: "Formulación", Icono: "forward", Url: "pages/formulacion", Opciones: null },
+      { Nombre: "Evaluación", Icono: "done_all", Url: "pages/evaluacion", Opciones: null },
+      { Nombre: "Seguimiento", Icono: "find_in_page", Url: "pages/seguimiento", Opciones: null },  
+    ] }
+  ]},  
+  { Nombre: "Banco de Proyectos", Icono: "forward", Url: null, Opciones: null },
+  { Nombre: "Programacion Presupuestal", Icono: "done_all", Url: null, Opciones: null },
 ]; 
 
 localStorage.setItem('menu', btoa(JSON.stringify(menu)));
