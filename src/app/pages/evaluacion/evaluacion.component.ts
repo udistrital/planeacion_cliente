972569045839ec9b57f66c4d76e9744df638c430
JoseAlejoGonza <jose_alejogonza@hotmail.com>
2022-02-02 20:04:50 -0500

    feat:implementación de mockups de evaluación

--- src/app/pages/evaluacion/evaluacion.component.ts
+++ src/app/pages/evaluacion/evaluacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestManager } from '../services/requestManager';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-evaluacion',
@@ -7,7 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluacionComponent implements OnInit {
 
-  constructor() { }
+  tipoPlanId: string; // id tipo plan
+  idPadre: string; // id padre del objeto
+  planes: any[];
+
+  constructor(
+    private request: RequestManager,
+  ) { 
+    this.loadPlanes(); 
+  }
+
+  onChange(plan){
+    if (plan == undefined){
+      this.tipoPlanId = undefined;
+    } else {
+      this.tipoPlanId = plan.tipo_plan_id;
+      this.idPadre = plan._id; // id plan
+    }
+  }
+
+  loadPlanes(){
+    this.request.get(environment.PLANES_CRUD, `plan?query=formato:true`).subscribe((data: any) => {
+      if (data){
+        this.planes = data.Data;
+        this.planes = this.filterActivos(this.planes);
+      }
+    },(error) => {
+      Swal.fire({
+        title: 'Error en la operación', 
+        text: 'No se encontraron datos registrados',
+        icon: 'warning',
+        showConfirmButton: false,
+        timer: 2500
+      })
+    })
+  }
+
+  filterActivos(data) {
+    return data.filter(e => e.activo == true);
+  }
 
   ngOnInit(): void {
   }
