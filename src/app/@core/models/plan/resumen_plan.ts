ffb8479bf4bbb15023f68ea0534db2000647e597
felimarod <felipemartinrod@gmail.com>
2024-02-04 21:15:53 -0500

    feat: Se agrega la tabla resumen para formulacion

---
+++ src/app/@core/models/plan/resumen_plan.ts
@@ -0,0 +1,12 @@
+export interface ResumenPlan {
+  dependencia_id: string;
+  dependencia_nombre: string;
+  estado: string;
+  estado_id: string;
+  id: string;
+  nombre: string;
+  ultima_edicion: Date;
+  version: number;
+  vigencia: number;
+  vigencia_id: string;
+}
