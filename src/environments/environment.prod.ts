a3ec7f1616c599a9e18a0bd202d8cb3e1253d908
Brayan Sierra <36229498+a52290451@users.noreply.github.com>
2024-02-16 22:10:53 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -24,6 +24,7 @@ export const environment = {
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
   NOTIFICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/",
+  ARM_AWS_NOTIFICACIONES:"arn:aws:sns:us-east-1:699001025740:test-Cumplidos",
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: '1IPIIJhKm_PVOPPR80Gnzjvg6Roa',
