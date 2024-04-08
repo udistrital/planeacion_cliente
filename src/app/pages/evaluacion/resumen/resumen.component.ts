eb2541abf7c23838379cb6f80a3169917ef92ce8
felimarod <felipemartinrod@gmail.com>
2024-04-07 23:56:36 -0500

    feat: evaluacion filtro periodo

--- src/app/pages/evaluacion/resumen/resumen.component.ts
+++ src/app/pages/evaluacion/resumen/resumen.component.ts
@@ -14,6 +14,9 @@ export class ResumenComponent implements OnInit {
   @Input() idVigencia: string;
   @Input() plan: Dato;
   @Input() unidades: any[];
+  @Input() periodo: any;
+  unidadesConPeriodoSeleccionado = [];
+
   columnType = 'ColumnChart';
   lineChartOptions = {
     title: 'Avance ponderado por planes',
@@ -21,27 +24,152 @@ export class ResumenComponent implements OnInit {
     hAxis: { title: 'Unidad' },
     vAxis: { title: 'Avance', minValue: 0, maxValue: 100 },
   };
-
-  lineChartColumnNames = [
-    'Unidad',
-    'Avance',
-    { role: 'style' },
-  ];
-
+  lineChartColumnNames = ['Unidad', 'Avance', { role: 'style' }];
   lineChartData = [['', 0, 'color: rgb(143, 27, 0)']];
   auxLineChartData = [];
 
   infoTabla = [];
   mostrarTabla: boolean = false;
+  mostrarGrafica: boolean = false;
 
   constructor(private request: RequestManager) {}
 
   async ngOnInit() {
-    await this.calcularAvanceGeneral();
+    if (this.periodo === 'TODOS') {
+      await this.calcularAvanceGeneral();
+      this.mostrarTabla = true;
+      this.mostrarGrafica = true;
+    } else {
+      this.mostrarTabla = false;
+      await this.obtenerUnidadesConPeriodo();
+      this.unidades = this.unidadesConPeriodoSeleccionado;
+      if (this.unidades.length === 0) {
+        Swal.fire({
+          title:
+            'No se cuenta con registros asociados a los trimestres buscados',
+          text: `No existen proyectos con registros en fase de seguimiento asociados a el trimestre seleccionado`,
+          icon: 'warning',
+          showConfirmButton: true,
+        });
+      } else {
+        await this.calcularAvanceGeneral();
+        this.mostrarGrafica = true;
+      }
+    }
+  }
+
+  async obtenerUnidadesConPeriodo() {
+    Swal.fire({
+      title: 'Cargando Periodos',
+      timerProgressBar: true,
+      showConfirmButton: false,
+      willOpen: () => {
+        Swal.showLoading();
+      },
+    });
+    for (let index = 0; index < this.unidades.length; index++) {
+      const unidad = this.unidades[index];
+      await new Promise((resolve) => {
+        this.request
+          .get(
+            environment.PLANES_MID,
+            `evaluacion/planes_periodo/${this.idVigencia}/${unidad.Id}`
+          )
+          .subscribe((data: any) => {
+            if (data?.Data) {
+              for (let pos = 0; pos < data.Data.length; pos++) {
+                const elemento = data.Data[pos];
+                if (elemento['plan'] === this.plan.nombre) {
+                  elemento['periodos'].forEach((periodo) => {
+                    if (
+                      `${periodo.nombre[0].toUpperCase()}${periodo.nombre
+                        .substring(1)
+                        .toLowerCase()}` === this.periodo.nombre
+                    ) {
+                      this.unidadesConPeriodoSeleccionado.push(unidad);
+                    }
+                  });
+                }
+              }
+              resolve(this.unidadesConPeriodoSeleccionado);
+            }
+          });
+      });
+    }
+    Swal.close();
+  }
+
+  async obtenerDatosGraficaPorPeriodos(unidad: { Id: string; Nombre: string }) {
+    await new Promise((resolve, reject) => {
+      this.request
+        .get(
+          environment.PLANES_MID,
+          `evaluacion/avance/${this.plan.nombre}/${this.idVigencia}/${unidad.Id}`
+        )
+        .subscribe(
+          (data: any) => {
+            if (data) {
+              let auxDataBarra: [string, number, string] = ['', 0, ''];
+              auxDataBarra[0] = unidad.Nombre;
+              let color: string;
+              let avance = 0;
+              switch (this.periodo.nombre) {
+                case 'Trimestre uno':
+                  avance = data.Data.Trimestres['1'];
+                  break;
+                case 'Trimestre dos':
+                  avance = data.Data.Trimestres['2'];
+                  break;
+                case 'Trimestre tres':
+                  avance = data.Data.Trimestres['3'];
+                  break;
+                case 'Trimestre cuatro':
+                  avance = data.Data.Trimestres['4'];
+                  break;
+                default:
+                  break;
+              }
+
+              if (avance <= 20) {
+                //  0,0 -  20  % #c50820
+                color = '#c50820';
+              } else if (avance > 20 && avance <= 40) {
+                // 20,1 -  40  % #faa99c
+                color = '#faa99c';
+              } else if (avance > 40 && avance <= 60) {
+                // 40,1 -  60  % #fac11d
+                color = '#fac11d';
+              } else if (avance > 60 && avance <= 80) {
+                // 60,1 -  80  % #fdff21
+                color = '#fdff21';
+              } else {
+                // 80,1 - 100  % #73af49
+                color = '#73af49';
+              }
+              auxDataBarra[1] = avance;
+              auxDataBarra[2] = `color: ${color}`;
+
+              this.auxLineChartData.push(auxDataBarra);
+              resolve(auxDataBarra);
+            }
+          },
+          (error) => {
+            Swal.close();
+            Swal.fire({
+              title: 'Error al obtener los datos',
+              text: `No se encontró el avance de la Unidad`,
+              icon: 'warning',
+              showConfirmButton: false,
+              timer: 2500,
+            });
+            reject(error);
+          }
+        );
+    });
   }
 
   async obtenerDatosUnidad(unidad: { Id: string; Nombre: string }) {
-    return await new Promise((resolve, reject) => {
+    await new Promise((resolve, reject) => {
       this.request
         .get(
           environment.PLANES_MID,
@@ -102,7 +230,7 @@ export class ResumenComponent implements OnInit {
     });
   }
 
-  async calcularAvanceGeneral() {
+  async calcularAvanceGeneral(unidades = this.unidades) {
     Swal.fire({
       title: 'Cargando datos de las unidades...',
       allowEscapeKey: false,
@@ -112,13 +240,18 @@ export class ResumenComponent implements OnInit {
       },
     });
     this.auxLineChartData = [];
-    for (let index = 0; index < this.unidades.length; index++) {
-      await this.obtenerDatosUnidad(this.unidades[index]);
+    if (this.periodo === 'TODOS') {
+      for (let index = 0; index < unidades.length; index++) {
+        await this.obtenerDatosUnidad(unidades[index]);
+      }
+    } else {
+      for (let index = 0; index < unidades.length; index++) {
+        await this.obtenerDatosGraficaPorPeriodos(unidades[index]);
+      }
     }
     Swal.close();
     if (this.auxLineChartData.length > 0) {
       this.lineChartData = this.auxLineChartData;
     }
-    this.mostrarTabla = true
   }
 }
