e827a83aa06b61893c22725218246ba04d05a924
diegobauto <diegobauto00@gmail.com>
2024-05-16 14:02:58 -0500

    feat: Variables de entorno udistrital/planeacion_cliente/issues/1035

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -42,10 +42,8 @@ export const environment = {
   CONFIGURACION_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
   CONF_MENU_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/menu_opcion_padre/ArbolMenus/',
   NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
-
   NOTIFICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/",
   ARM_AWS_NOTIFICACIONES: "arn:aws:sns:us-east-1:699001025740:test-planeacion_cliente_sns.fifo",
-
   TOKEN: {
     AUTORIZATION_URL: 'https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize',
     CLIENTE_ID: 'e36v1MPQk2jbz9KM4SmKhk8Cyw0a',
